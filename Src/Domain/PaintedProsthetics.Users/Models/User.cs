5a0addeda8af10b54b2885d319246550dbf17918
Max <hellhunter15@hotmail.com>
2020-07-05 05:36:07 +0200

    amputee and user model start.

---
+++ Src/Domain/PaintedProsthetics.Users/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaintedProsthetics.Users.Models
+{
+    public class User
+    {
+        public string _FirstName { get; set; }
+        public string _LastName { get; set; }
+        public DateTime _BirthDate { get; set; }
+        public string _Password { get; set; }
+        public string _Email { get; set; }
+    }
+}
