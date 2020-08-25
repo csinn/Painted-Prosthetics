a38ecf46bd3b0e5a23b009bfec45e38170c9ba83
Max <hellhunter15@hotmail.com>
2020-08-25 05:45:18 +0200

    removed unused code in Button.

--- React/src/Components/Button.js
+++ React/src/Components/Button.js
@@ -1,31 +1,14 @@
 import React from 'react'
 import './Button.css'
 
-const STYLES = [
-    'btn--primary',
-    'btn--outline'
-]
-
-const SIZES = [
-    'btn--medium',
-    'btn--large'
-]
-
 export const Button = ({
     children,
     type,
     onClick,
-    buttonStyle,
-    buttonSize
 }) => {
-
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
-
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize :  SIZES[0]
-
     return(
         <button 
-            className={'btn ${checkButtonStyle} ${checkButtonSize}'} 
+            className={'btn btn--primary btn--medium'} 
             onClick = {onClick}
             type= {type}
             >
