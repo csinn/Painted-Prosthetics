4fd1411f958a887693671f55b7f06e009541a505
Max <hellhunter15@hotmail.com>
2020-08-25 05:21:32 +0200

    made a start to the react nav bar.

---
+++ React/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./Components/Navbar/Navbar";
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Gallery',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Info',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Donate',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
